71f18ee2c30c5168dc6f54f3d60a0ecc03cd2592
Till Blume <tbl@informatik.uni-kiel.de>
2020-01-06 11:55:26 +0100

    reduced logger intervaal to 10,000,000

--- code/IMPULSE/src/main/java/utils/MemoryTracker.java
+++ code/IMPULSE/src/main/java/utils/MemoryTracker.java
@@ -16,7 +16,7 @@ import static main.java.utils.MainUtils.createFile;
 
 public class MemoryTracker implements IQuintListener {
     private static final Logger logger = LogManager.getLogger(MemoryTracker.class.getSimpleName());
-    private static final int loggingInterval = 500000;
+    private static final int loggingInterval = 10000000;
     private PrintStream out;
     private long n = 0;
     private long lastTime = System.currentTimeMillis();
